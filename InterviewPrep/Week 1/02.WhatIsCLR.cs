eb0363b6784501a2d0a8db25985cac06125a6629
AtharvaD@11 <deshpandeatharva001@gmail.com>
2026-05-10 21:30:15 +0530

    10-05-26

---
+++ InterviewPrep/Week 1/02.WhatIsCLR.cs
@@ -0,0 +1,19 @@
+﻿/*
+ 
+What is CLR? (Common Language Runtime)
+
+ CLR is the execution engine that runs our .NET apps.
+When we compile our C# code, it directly doesn't get compiled into machine code.
+Instead, it is compiled into an IL. The CLR then takes this IL and using a JIT compiler, converts it into a native machine code.
+The CPU then executes this machine code.
+
+Apart from code execution, it provides set of services like:
+* memory management and garbage collection
+* exception handling
+* type safety
+* networking
+* security
+* thread management
+
+
+ */
